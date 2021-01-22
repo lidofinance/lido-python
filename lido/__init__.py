d7d8aeb2db46610cd00eaa4fe6a8fa03293da834
kolyasapphire <a@ks.gg>
2021-01-22 13:34:52 +0300

    Preparation for bumpversion for releases

--- lido/__init__.py
+++ lido/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.1.0"
+
 from lido.get_operators_data import get_operators_data  # noqa: F401
 from lido.get_operators_keys import get_operators_keys  # noqa: F401
 from lido.validate_keys import validate_keys_mono  # noqa: F401
