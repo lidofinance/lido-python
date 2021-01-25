e083af5e4351c6372f8266664a91350d5ce23a73
kolyasapphire <a@ks.gg>
2021-01-25 21:13:01 +0300

    Automatic w3 middleware for Geth

--- lido/utils/data_actuality.py
+++ lido/utils/data_actuality.py
@@ -1,5 +1,11 @@
 from web3.auto import w3
 
+# Inject middleware for geth if we are on Goerli
+if w3.eth.chainId == 5:
+    from web3.middleware import geth_poa_middleware
+
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
 
 def get_data_actuality():
     return {
