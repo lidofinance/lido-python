08ee832155d87d6e61bd4cb7194b9b05738eee05
kolyasapphire <a@ks.gg>
2021-02-03 15:55:10 +0300

    Signature validation module rework for CLI

--- lido/__init__.py
+++ lido/__init__.py
@@ -5,6 +5,7 @@ from lido.get_operators_keys import get_operators_keys  # noqa: F401
 from lido.validate_keys import validate_key  # noqa: F401
 from lido.validate_keys import validate_keys_mono  # noqa: F401
 from lido.validate_keys import validate_keys_multi  # noqa: F401
+from lido.validate_keys import validate_key_list_multi  # noqa: F401
 from lido.find_duplicates import find_duplicates  # noqa: F401
 from lido.find_duplicates import spot_duplicates  # noqa: F401
 from lido.fetch_and_validate import fetch_and_validate  # noqa: F401
--- lido/validate_keys.py
+++ lido/validate_keys.py
@@ -12,19 +12,16 @@ from lido.contracts.w3_contracts import get_lido_contract
 
 import concurrent
 
-# Prepare network
-lido = get_lido_contract()
-withdrawal_credentials = bytes(lido.functions.getWithdrawalCredentials().call())
-fork_version = get_chain_setting(get_eth2_chain_name()).GENESIS_FORK_VERSION
-domain = compute_deposit_domain(fork_version=fork_version)
 
-
-def validate_key(key: t.Dict) -> bool:
+def validate_key(data: t.Dict) -> t.Optional[bool]:
     """Run signature validation on a key"""
 
-    # Is this key already validated? eg in cache
+    key = data["key"]
+    withdrawal_credentials = data["withdrawal_credentials"]
+
+    # Is this key already validated?
     if "valid_signature" in key.keys():
-        return
+        return None
 
     pubkey = bytes.fromhex(key["key"]) if type(key["key"]) is str else key["key"]
     signature = (
@@ -32,6 +29,9 @@ def validate_key(key: t.Dict) -> bool:
         if type(key["depositSignature"]) is str
         else key["depositSignature"]
     )
+
+    fork_version = get_chain_setting(get_eth2_chain_name()).GENESIS_FORK_VERSION
+    domain = compute_deposit_domain(fork_version=fork_version)
 
     # Minimum staking requirement of 32 ETH per validator
     REQUIRED_DEPOSIT_ETH = 32
@@ -49,31 +49,92 @@ def validate_key(key: t.Dict) -> bool:
     return bls.Verify(pubkey, signing_root, signature)
 
 
-def validate_keys_mono(operators):
-    """This is an additional, single-process key validation function"""
+def validate_keys_mono(
+    operators: t.List[t.Dict],
+    lido_address: t.Optional[str] = None,
+    lido_abi_path: t.Optional[str] = None,
+) -> t.List[t.Dict]:
+    """
+    This is an additional, single-process key validation function.
+    Modifies the input! Adds "valid_signature" field to every key item.
+    """
+
+    # Prepare network vars
+    lido = get_lido_contract(address=lido_address, path=lido_abi_path)
+    withdrawal_credentials = bytes(lido.functions.getWithdrawalCredentials().call())
 
     for op_i, op in enumerate(operators):
         for key_i, key in enumerate(op["keys"]):
-            # Is this key already validated? eg in cache
+            # Is this key already validated?
             if "valid_signature" not in key.keys():
-                operators[op_i]["keys"][key_i]["valid_signature"] = validate_key(key)
+                operators[op_i]["keys"][key_i]["valid_signature"] = validate_key(
+                    {"key": key, "withdrawal_credentials": withdrawal_credentials}
+                )
 
     return operators
 
 
-def validate_keys_multi(operators):
+def validate_keys_multi(
+    operators: t.List[t.Dict],
+    lido_address: t.Optional[str] = None,
+    lido_abi_path: t.Optional[str] = None,
+) -> t.List[t.Dict]:
     """
     Main multi-process validation function.
     Modifies the input! Adds "valid_signature" field to every key item.
     It will spawn an appropriate process pool for the amount of threads on processor.
     """
 
+    # Prepare network vars
+    lido = get_lido_contract(address=lido_address, path=lido_abi_path)
+    withdrawal_credentials = bytes(lido.functions.getWithdrawalCredentials().call())
+
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for op_i, op in enumerate(operators):
-            validate_key_results = executor.map(validate_key, op["keys"])
+
+            # Pass {key,withdrawal_credentials} to overcome 1-arg limit of concurrency.map()
+            arguments = [
+                {"key": key, "withdrawal_credentials": withdrawal_credentials} for key in op["keys"]
+            ]
+
+            validate_key_results = executor.map(validate_key, arguments)
+
             for key_index, validate_key_result in enumerate(validate_key_results):
-                # Is this key already validated? eg in cache
+                # Is this key already validated?
                 if validate_key_result is not None:
                     operators[op_i]["keys"][key_index]["valid_signature"] = validate_key_result
 
     return operators
+
+
+def validate_key_list_multi(
+    input: t.List[t.Dict],
+    lido_address: t.Optional[str] = None,
+    lido_abi_path: t.Optional[str] = None,
+) -> t.List[t.Dict]:
+    """
+    Additional multi-process validation function.
+    It returns invalid keys instead of the whole operator data like other functions.
+    """
+
+    # Prepare network
+    lido = get_lido_contract(address=lido_address, path=lido_abi_path)
+    withdrawal_credentials = bytes(lido.functions.getWithdrawalCredentials().call())
+
+    invalid = []
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+
+        # Pass {key,withdrawal_credentials} to overcome 1-arg limit of concurrency.map()
+        arguments = [
+            {"key": key, "withdrawal_credentials": withdrawal_credentials} for key in input
+        ]
+
+        validate_key_results = executor.map(validate_key, arguments)
+
+        for key_index, validate_key_result in enumerate(validate_key_results):
+            # Is this key already validated?
+            if validate_key_result is not None and validate_key_result is False:
+                invalid.append(input[key_index])
+
+    return invalid
