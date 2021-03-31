592b5882b8f96d3229482845662ba9bcf5a4fcae
kolyasapphire <a@ks.gg>
2021-03-31 11:48:29 +0300

    Version 0.2.5

--- lido/__init__.py
+++ lido/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 from lido.get_operators_data import get_operators_data  # noqa: F401
 from lido.get_operators_keys import get_operators_keys  # noqa: F401
