f1535df5dad7d70ff61249230dce70de848235cf
kolyasapphire <a@ks.gg>
2021-03-31 10:58:56 +0000

    Prater eth2 name fix

--- lido/constants/chains.py
+++ lido/constants/chains.py
@@ -14,6 +14,6 @@ def get_eth2_chain_name() -> str:
 
     # Return Pyrmont for Goerli deployment
     if w3.eth.chainId == 5:
-        return "pyrmont"
+        return "prater"
 
     return chains[w3.eth.chainId]
