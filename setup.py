31c323945dc27af2378ba064e5314fb4f1167349
kolyasapphire <a@ks.gg>
2021-03-31 13:00:08 +0300

    Version 0.2.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(HERE, "README.md")) as fid:
 
 setup(
     name="Lido",
-    version="0.2.5",
+    version="0.2.6",
     description="Network helpers for Lido",
     long_description=README,
     long_description_content_type="text/markdown",
